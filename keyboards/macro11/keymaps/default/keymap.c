3c8be6ca78ed071f948eadf1915b632912e2b115
Thomas Baart <mail@thomasbaart.nl>
2019-01-10 21:44:01 +0100

    Added Macro11 handwired keyboard

---
+++ keyboards/macro11/keymaps/default/keymap.c
@@ -0,0 +1,40 @@
+/* Copyright 2018 REPLACE_WITH_YOUR_NAME
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [0] = LAYOUT( /* Base */
+    RESET, KC_B, \
+    RESET, RGB_HUD, RGB_SAI, RGB_HUI, RGB_VAI, \
+      KC_H,         RGB_SAD,          RGB_VAD
+  ),
+};
+
+void encoder_update_user(uint8_t index, bool clockwise) {
+    if (index == 0) {
+        if (clockwise) {
+            register_code(KC_PGDN);
+            unregister_code(KC_PGDN);
+        } else {
+            register_code(KC_PGUP);
+            unregister_code(KC_PGUP);
+        }
+    }
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  return true;
+}
---
+++ keyboards/macro11/keymaps/default/readme.md
@@ -0,0 +1,1 @@
+# The default keymap for macro11
